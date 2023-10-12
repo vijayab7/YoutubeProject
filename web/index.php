bfc83a69331699e4f8726bb492f10cc65fecb0c5
John Doe <vijayvjj7@gmail.com>
2023-10-12 11:12:57 +0900

    deleted comments

--- web/index.php
+++ web/index.php
@@ -215,9 +215,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             <button class="btn btn-secondary rounded-pill" type="button">戻る</button>
         </div>
     </form>
-    <!-- Optional JavaScript; choose one of the two! -->
 
-    <!-- Option 1: Bootstrap Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
         crossorigin="anonymous"></script>
